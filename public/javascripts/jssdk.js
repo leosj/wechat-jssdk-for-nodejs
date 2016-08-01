d77c6c9c6871f7130111d09654c9316f60a2d1c5
leosj <393068657@qq.com>
2016-08-01 11:27:52 +0800

    增加获取网络制式和定位接口

--- public/javascripts/jssdk.js
+++ public/javascripts/jssdk.js
@@ -320,4 +320,38 @@ wx.ready(function () {
 		});
 		
 	};
+	
+	//四：查看网络制式
+	document.querySelector('#showNetType').onclick = function() {
+		wx.getNetworkType({
+		    success: function (res) {
+		        var networkType = res.networkType; // 返回网络类型2g，3g，4g，wifi
+		        alert(networkType);
+		    }
+		});
+		
+	};
+	
+	//获取当前位置并定位
+	document.querySelector('#getLocation').onclick = function() {
+		wx.getLocation({
+		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+		    success: function (res) {
+		        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+		        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+		        var speed = res.speed; // 速度，以米/每秒计
+		        var accuracy = res.accuracy; // 位置精度
+		        
+		        wx.openLocation({
+				    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+				    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+				    name: '我的位置', // 位置名
+				    address: '我的位置', // 地址详情说明
+				    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+				    infoUrl: 'http://www.baidu.com/' // 在查看位置界面底部显示的超链接,可点击跳转
+				});
+		    }
+		});
+		
+	};
 });
